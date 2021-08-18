d057cea6fc384bca596c988672a27dc11a39a881
siddharth <siddharth.dumbre@somaiya.edu>
2021-08-18 11:22:14 +0530

    Added Splash Screen

--- app/src/main/java/com/example/educationapp/MainActivity.kt
+++ app/src/main/java/com/example/educationapp/MainActivity.kt
@@ -18,46 +18,12 @@ class MainActivity : AppCompatActivity() {
         var gson = Gson()
         val jsonString = getJSONFromAssets(this)
         tvJsonString.text = jsonString
-
-        //Talent Test Page
-
-//        val talent_Test_Model = gson.fromJson(
-//            jsonString,
-//            TalentTestListClass::class.java
-//        )
-
-        //Exams After Intermediate
-
-//        val exam_after_intermediate_model = gson.fromJson(
-//            jsonString,
-//            ExamAfterIntermediateListClass::class.java
-//        )
-
-        //Course Type
-
-//        val Course_Type_Model = gson.fromJson(
-//            getJSONFromAssets(this),
-//            CourseTypeListClass::class.java
-//        )
-
-        //After Intermediate
-
-//        val After_Intermedaite_Model = gson.fromJson(
-//            getJSONFromAssets(this),
-//            AfterIntermediateListClass::class.java
-//        )
-
-        //After Graduation
-
-//        val After_Graduation_Model = gson.fromJson(
-//            getJSONFromAssets(this),
-//            AfterGraduationListClass::class.java
-//        )
-
-        //After Tenth
-
-        val After_Tenth_Model = gson.fromJson(
-            getJSONFromAssets(this),
+        val talentModel = gson.fromJson(
+            jsonString,
+            TalentTestListClass::class.java
+        )
+        val AfterTenModel = gson.fromJson(
+            getJSONFromAssets1(this),
             AfterTenListClass::class.java
         )
         Log.d("Main Activity","Size:")
@@ -65,6 +31,21 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun getJSONFromAssets(context: Context): String? {
+        var json: String? = null
+        try {
+            val myUsersJSONFile = assets.open("talent_test.json")
+            val size = myUsersJSONFile.available()
+            val buffer = ByteArray(size)
+            myUsersJSONFile.read(buffer)
+            myUsersJSONFile.close()
+            json = String(buffer)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return null
+        }
+        return json
+    }
+    private fun getJSONFromAssets1(context: Context): String? {
         var json: String? = null
         try {
             val myUsersJSONFile = assets.open("After10th.json")
