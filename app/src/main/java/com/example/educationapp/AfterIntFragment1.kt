f65747301587a8a4c15dba9c0bf052ffec110516
siddharth <siddharth.dumbre@somaiya.edu>
2021-08-30 16:14:15 +0530

    Expandable list view working in 1st fragment

--- app/src/main/java/com/example/educationapp/AfterIntFragment1.kt
+++ app/src/main/java/com/example/educationapp/AfterIntFragment1.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListView
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_after_int1.*
+import java.io.IOException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,14 +37,56 @@ class AfterIntFragment1 : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        var gson = Gson()
-        val main = MainActivity()
-
-//        val jsonString = main.getJSONFromAssets(this,"AfterIntermediate.json")
+//        var gson = Gson()
+//        val main = MainActivity()
+//
+//        val jsonString = main.getJSONFromAssets("AfterIntermediate","AfterIntermediate.json")
 //        val after_Intermediate_Model = gson.fromJson(
 //            jsonString,
 //            AfterIntermediateListClass::class.java
 //        )
+
+//        var l1:ArrayList<String> =ArrayList<String>()
+//        var l2:ArrayList<String> = ArrayList<String>()
+//        var l2_1:ArrayList<String> = ArrayList<String>()
+//        var l2_2:ArrayList<String> = ArrayList<String>()
+//        var l3:HashMap<String,ArrayList<String>> =HashMap()
+//        l1.add("Animals")
+//        l1.add("Birds")
+//        l1.add("fishes")
+//        l2.add("Dog")
+//        l2.add("cat")
+//        l2.add("tiger")
+//        l2_1.add("peacock")
+//        l2_1.add("Ostrich")
+//        l2_1.add("pegion")
+//        l3[l1[0]]=l2
+//        l3[l1[1]]=l2_1
+//        l2_2.add("Bombayduck")
+//        l2_2.add("prompet")
+//        l3[l1[2]]=l2_2
+//        val afInt=AfterIntermediate()
+//        listadapter= Course_Type_Adapter(this as AfterIntermediate,l1,l3)
+//        elistFrag1.setAdapter(listadapter)
+
+
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var gson = Gson()
+        val main = MainActivity()
+        val jsonString = main.getJSONFromAssets(activity!!,"AfterIntermediate.json")
+        val after_Intermediate_Model = gson.fromJson(
+            jsonString,
+            AfterIntermediateListClass::class.java
+        )
+
+        val view=inflater.inflate(R.layout.fragment_after_int1, container, false)
 
         var l1:ArrayList<String> =ArrayList<String>()
         var l2:ArrayList<String> = ArrayList<String>()
@@ -64,19 +108,30 @@ class AfterIntFragment1 : Fragment() {
         l2_2.add("prompet")
         l3[l1[2]]=l2_2
         val afInt=AfterIntermediate()
-//        listadapter= Course_Type_Adapter(activity!!,l1,l3)
+
+        var Flist:ArrayList<String> =ArrayList<String>()
+        var Slist:ArrayList<String> = ArrayList<String>()
+        var Tlist:HashMap<String,ArrayList<String>> = HashMap()
+
+        try{
+            var i:Int=0
+            for(i in 0 until after_Intermediate_Model.data[0].streams.size) {
+                Flist.add(after_Intermediate_Model.data[0].streams[i].name)
+                var Slist:ArrayList<String> = after_Intermediate_Model.data[0].streams[i].course[0].sub_course
+                Tlist[Flist[i]]=Slist
+            }
+
+
+        }catch (ex: IOException){
+            ex.printStackTrace()
+        }
+
+        listadapter= Course_Type_Adapter(activity!!,Flist,Tlist)
 //        elistFrag1.setAdapter(listadapter)
 
+        view.findViewById<ExpandableListView>(R.id.elistFrag1).setAdapter(listadapter)
 
-    }
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_after_int1, container, false)
+        return view
     }
 
     companion object {
